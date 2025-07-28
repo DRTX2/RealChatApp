a9eff3bfff9b7c56184c1ddc61b5cb19df563b5c
David Manjarres <davidmanjarres2004@gmail.com>
2025-07-27 23:14:37 -0500

    Edit core models

---
+++ src/main/java/com/drtx/example/realchatapp/core/models/MessageType.java
@@ -0,0 +1,16 @@
+package com.drtx.example.realchatapp.core.models;
+
+public enum MessageType {
+    CHAT,
+    NOTIFICATION,
+    SYSTEM,
+    FILE,
+    IMAGE,
+    VIDEO,
+    AUDIO,
+    LOCATION,
+    REACTION,
+    STICKER,
+    EMOJI,
+    CUSTOM;
+}
--- src/main/java/com/drtx/example/realchatapp/core/models/User.java
+++ src/main/java/com/drtx/example/realchatapp/core/models/User.java
@@ -27,6 +27,14 @@ public class User {
         this.messages=messages;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getUsername() {
         return username;
     }
